6ca78b8ea9d0ff7d24c13019d557bb31716e9bce
Jorge Riberi <jariberi@gmail.com>
2016-12-04 17:53:22 -0300

    Cambio de clientes SOAP: Suds

--- wsaa.py
+++ wsaa.py
@@ -3,13 +3,16 @@
 "Módulo para obtener un ticket de autorización del web service WSAA de AFIP"
 
 # Basado en wsaa.py de Mariano Reingart
+import random
+import datetime
+from datetime import timedelta
+from django.utils.timezone import now
+import xml.etree.ElementTree as et
+from suds import WebFault
+import email, os, sys, warnings
+from base import WebServiceAFIP
 
 __author__ = "Jorge Riberi (jariberi@gmail.com)"
-
-import email, os, sys, warnings
-from php import date
-from pysimplesoap.client import SimpleXMLElement
-from base import WebServiceAFIP
 
 try:
     from M2Crypto import BIO, Rand, SMIME, SSL
@@ -20,40 +23,43 @@ except ImportError:
     from subprocess import Popen, PIPE
     from base64 import b64encode
 
-# Constantes (si se usa el script de linea de comandos)
-CERT_PROD = "C:\bases\prod.crt"  # El certificado X.509 obtenido de Seg. Inf.
-CERT_TEST = "C:\bases\test.crt"  # El certificado X.509 obtenido de Seg. Inf.
-PRIVATEKEY = "C:\bases\key.key"  # La clave privada del certificado CERT
 SERVICE = "wsfe"  # El nombre del web service al que se le pide el TA
 
-# WSAAURL: la URL para acceder al WSAA, verificar http/https y wsaa/wsaahomo
 WSAAURL_PROD = "https://wsaa.afip.gov.ar/ws/services/LoginCms?wsdl"  # PRODUCCION!!!
 WSAAURL_TEST = "https://wsaahomo.afip.gov.ar/ws/services/LoginCms?wsdl"  # homologacion (pruebas)
 
-# Verificación del web server remoto, necesario para verificar canal seguro
-CACERT = "conf/afip_ca_info.crt"  # WSAA CA Cert (Autoridades de Confiaza)
 
-
+CERT_FILE_PROD = "C:\bases\produccion.crt"
+CERT_FILE_TEST = "C:\bases\test.crt"
+PRIVATE_KEY_FILE = "C:\bases\key.key"
 # No debería ser necesario modificar nada despues de esta linea
 
 def create_tra(service=SERVICE, ttl=2400):
     "Crear un Ticket de Requerimiento de Acceso (TRA)"
-    tra = SimpleXMLElement(
-        '<?xml version="1.0" encoding="UTF-8"?>'
-        '<loginTicketRequest version="1.0">'
-        '</loginTicketRequest>')
-    tra.add_child('header')
+    tra = et.fromstring('<?xml version="1.0" encoding="UTF-8"?>'
+                        '<loginTicketRequest version="1.0">'
+                        '</loginTicketRequest>')
+    # tra = SimpleXMLElement(
+    #     '<?xml version="1.0" encoding="UTF-8"?>'
+    #     '<loginTicketRequest version="1.0">'
+    #     '</loginTicketRequest>')
+    header = et.SubElement(tra, 'header')
     # El source es opcional. Si falta, toma la firma (recomendado).
     # tra.header.addChild('source','subject=...')
     # tra.header.addChild('destination','cn=wsaahomo,o=afip,c=ar,serialNumber=CUIT 33693450239')
-    tra.header.add_child('uniqueId', str(date('U')))
-    tra.header.add_child('generationTime', str(date('c', date('U') - ttl)))
-    tra.header.add_child('expirationTime', str(date('c', date('U') + ttl)))
-    tra.add_child('service', service)
-    return tra.as_xml()
+    et.SubElement(header, 'uniqueId').text = str(random.randint(0, 999999))
+    # header.add_child('uniqueId', str(long((time.time()*1000))))
+    et.SubElement(header, 'generationTime').text = (datetime.datetime.now() - timedelta(seconds=120)).strftime("%Y-%m-%dT%H:%M:%S")
+    # tra.header.add_child('generationTime', str(date('c', date('U') - ttl)))
+    et.SubElement(header, 'expirationTime').text = (datetime.datetime.now() + timedelta(seconds=ttl)).strftime(
+        "%Y-%m-%dT%H:%M:%S")
+    # tra.header.add_child('expirationTime', str(date('c', date('U') + ttl)))
+    et.SubElement(tra, 'service').text = service
+    # tra.add_child('service', service)
+    return '<?xml version="1.0" encoding="UTF-8"?>' + et.tostring(tra)
 
 
-def sign_tra(tra, cert=CERT_TEST, privatekey=PRIVATEKEY, passphrase=""):
+def sign_tra(tra, cert=None, privatekey=None, passphrase=""):
     "Firmar PKCS#7 el TRA y devolver CMS (recortando los headers SMIME)"
 
     if BIO:
@@ -110,12 +116,13 @@ def sign_tra(tra, cert=CERT_TEST, privatekey=PRIVATEKEY, passphrase=""):
 class WSAA(WebServiceAFIP):
     "Interfaz para el WebService de Autenticación y Autorización"
 
-    def __init__(self, reintentos=1, produccion=False):
-        WebServiceAFIP.__init__(self, reintentos, produccion)
+    def __init__(self, produccion=False):
+        WebServiceAFIP.__init__(self, produccion)
 
     def CreateTRA(self, service="wsfe", ttl=2400):
         "Crear un Ticket de Requerimiento de Acceso (TRA)"
-        return create_tra(service, ttl)
+        tra = create_tra(service, ttl)
+        return tra
 
     def SignTRA(self, tra, cert, privatekey, passphrase=""):
         "Firmar el TRA y devolver CMS"
@@ -123,31 +130,31 @@ class WSAA(WebServiceAFIP):
 
     def Conectar(self):
         wsdl = WSAAURL_PROD if self.produccion else WSAAURL_TEST
-        return WebServiceAFIP.Conectar(self, wsdl=wsdl, proxy="", wrapper=None, cacert=None, timeout=30,
-                                       soap_server=None)
+        return WebServiceAFIP.Conectar(self, wsdl=wsdl)
 
     def LoginCMS(self, cms):
         "Obtener ticket de autorización (TA)"
-        results = self.client.loginCms(in0=str(cms))
-        ta_xml = results['loginCmsReturn'].encode("utf-8")
-        self.xml = ta = SimpleXMLElement(ta_xml)
-        self.Token = str(ta.credentials.token)
-        self.Sign = str(ta.credentials.sign)
-        self.ExpirationTime = str(ta.header.expirationTime)
-        return ta_xml
+        try:
+            xml=et.fromstring(self.client.service.loginCms(in0=str(cms)))
+            self.Token = xml[1][0].text
+            self.Sign = xml[1][1].text
+            self.ExpirationTime = xml[0][4].text
+            return True
+        except WebFault, e:
+            self.excCode = e.fault.faultcode
+            self.excMsg = e.fault.faultstring
+            return False
 
 
-def obtener_permiso(ttl=120, servicio="wsfe", produccion=False):##Ruso: Factura electronica
-    wsaa = WSAA(produccion=produccion)
-    tra = wsaa.CreateTRA(service=servicio, ttl=ttl)
-    cert = CERT_PROD if produccion else CERT_TEST
-    cms = wsaa.SignTRA(tra, cert=cert, privatekey=PRIVATEKEY)
-    try:
+def obtener_o_crear_permiso(ttl=120, servicio="wsfe", produccion=False):  ##Ruso: Factura electronica
+        wsaa = WSAA(produccion=produccion)
+        tra = wsaa.CreateTRA(service=servicio, ttl=ttl)
+        cert = CERT_FILE_PROD if produccion else CERT_FILE_TEST
+        cms = wsaa.SignTRA(tra, cert=cert, privatekey=PRIVATE_KEY_FILE)
         if wsaa.Conectar():
-            wsaa.LoginCMS(cms)
-            return wsaa
+            if wsaa.LoginCMS(cms):
+                return True, wsaa
+            else:
+                return False, wsaa
         else:
-            return None
-    except Exception as e:
-        print e
-        return None
+            return False, wsaa
